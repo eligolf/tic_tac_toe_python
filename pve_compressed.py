917df1cf08cf50a5d8369e173fc91035dd705e02
eligolf <59540119+eligolf@users.noreply.github.com>
2021-01-15 09:15:53 +0100

    Add files via upload

--- pve_compressed.py
+++ pve_compressed.py
@@ -1,38 +1,40 @@
 def p(b):
-    b2 = [' ' if i == 0 else 'X' if i == -1 else 'O' if i == 1 else i for i in b]
-    [print(f'\n{b2[r*3:3+r*3]}') if r == 0 else print(b2[r*3:3+r*3]) for r in range(3)]
+    c = [' ' if i == 0 else 'X' if i == -1 else 'O' if i == 1 else i for i in b]
+    [print(f'\n{c[r*3:3+r*3]}') if r == 0 else print(c[r*3:3+r*3]) for r in range(3)]
 def e(b, t):
     for p in ([0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]):
         if b[p[0]] == b[p[1]] == b[p[2]] == t: return 1
-def nm(b, d, t):
-    if e(b, t) == 1: return None, (10+d)
-    if e(b, -t) == 1: return None, -(10 + d)
-    if 0 not in b: return None, 0
-    bs = -20
-    for m in [i for i in range(len(b)) if b[i] == 0]:
+def n(b, d, t):
+    if e(b, t): return 0, (9+d)
+    if e(b, -t): return 0, -(9 + d)
+    if 0 not in b: return 0, 0
+    x = -20
+    for m in [i for i in range(9) if not b[i]]:
         b[m] = t
-        score = -nm(b, d - 1, -t)[1]
+        s = -n(b, d - 1, -t)[1]
         b[m] = 0
-        if score > bs: bs, bm = score, m
-    return bm, bs
+        if s > x: x, y = s, m
+    return y, x
 def r():
-    b, ut = [0]*9, -1
+    b, w = [0]*9, -1
     p(b)
     while 1:
-        if ut == -1 and not e(b, ut) and not e(b, -ut) and 0 in b:
+        if w == -1 and not (e(b, w) or e(b, -w)) and 0 in b:
             while 1:
                 u = input('\nPlease enter your move (1-9): ')
-                if u.isnumeric() and int(u) - 1 in range(9) and b[int(u) - 1] == 0:
-                    b[int(u)-1], ut = -1, ut*-1
-                    p(b)
-                    break
-        elif ut == 1 and not e(b, ut) and not e(b, -ut) and 0 in b:
-            move, score = nm(b, 8, 1)
-            b[move], ut = 1, ut*-1
+                if u.isnumeric():
+                    u = int(u)-1
+                    if 0 <= u < 9 and not b[u]:
+                        b[u], w = -1, w*-1
+                        p(b)
+                        break
+        elif w == 1 and not (e(b, w) or e(b, -w)) and 0 in b:
+            move, score = n(b, 8, 1)
+            b[move], w = 1, w*-1
             p(b)
         else:
-            text = 'You won!' if e(b, -1) else 'AI won!' if e(b, 1) else 'Game drawn!'
-            if input(f'\n{text} Do you want to play again (y/n)? ').lower() != 'y': break
+            f = 'You won!' if e(b, -1) else 'AI won!' if e(b, 1) else 'Game drawn!'
+            if input(f'\n{f} Do you want to play again (y/n)? ') != 'y': break
             r()
             break
 r()
--- pvp_compressed.py
+++ pvp_compressed.py
@@ -1,6 +1,6 @@
 def p(b):
-    b2 = ['-' if i == 0 else 'O' if i == -1 else 'X' if i == 1 else i for i in b]
-    [print(f'\n{b2[r*3:3+r*3]}') if r == 0 else print(b2[r*3:3+r*3]) for r in range(3)]
+    c = ['-' if i == 0 else 'O' if i == -1 else 'X' if i == 1 else i for i in b]
+    [print(f'\n{c[r*3:3+r*3]}') if r == 0 else print(c[r*3:3+r*3]) for r in range(3)]
 def w(b):
     for l in ([0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]):
         if b[l[0]] == b[l[1]] == b[l[2]] != 0: return 1
@@ -9,9 +9,11 @@ def r():
     p(b)
     while 1:
         u = input('\nPlease enter your move (1-9): ')
-        if u.isnumeric() and int(u)-1 in range(9) and b[int(u)-1] == 0:
-            b[int(u)-1], t = t, t*-1
-            p(b)
+        if u.isnumeric():
+            u = int(u)-1
+            if u in range(9) and b[u] == 0:
+                b[u], t = t, t*-1
+                p(b)
         if w(b) or 0 not in b:
             x = f'\n{"X" if t == -1 else "O"} won!' if w(b) else 'Game drawn!'
             if input(f'{x} Do you want to play again (y/n)? ') != 'y': break
