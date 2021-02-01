9eb20423cd3b05ee96b3d860ce44ffe75f2e2aa9
eligolf <59540119+eligolf@users.noreply.github.com>
2021-02-01 13:32:21 +0100

    Update pve_compressed.py

--- pve_compressed.py
+++ pve_compressed.py
@@ -21,6 +21,6 @@ def g():
             u=i(':')
             if u.isdigit():
                 u=int(u)-1
-                if u<9 and not b[u]:b[u],w=-1,-1
+                if u<9and not b[u]:b[u],w=-1,-1
         else:(m,s),b[m],w=n(b,8,1),1,1
 i=input;g()
