28e9826e973748e30e83e65a92cc1bd686f5d830
eligolf <59540119+eligolf@users.noreply.github.com>
2021-02-01 10:53:42 +0100

    Update pve_compressed.py

--- pve_compressed.py
+++ pve_compressed.py
@@ -1,6 +1,6 @@
 def p(b):[print(['X'if i<0 else'O'if i>0 else' 'for i in b][x*3:3+x*3])for x in r(3)]
 def e(b,t):
-    for i,d in (0,1),(3,1),(6,1),(0,3),(1,3),(2,3),(0,4),(2,2):
+    for i,d in(0,1),(3,1),(6,1),(0,3),(1,3),(2,3),(0,4),(2,2):
         if b[i]==b[i+d]==b[i+2*d]==t:return 1
 def n(b,d,t):
     if e(b,t):return 0,9
@@ -15,13 +15,13 @@ def n(b,d,t):
     return y,x
 def g():
     b,w=[0]*9,1
-    p(b)
     while 1:
         if all(b)or(e(b,w)or e(b,-w)):
             if i('?')!='y':break
             g()
             break
         if w>0:
+            p(b)
             u=i(':')
             if u.isdigit():
                 u=int(u)-1
@@ -31,6 +31,5 @@ def g():
         else:
             m,s=n(b,8,1)
             b[m],w=1,1
-            p(b)
 i,r=input,range
 g()
