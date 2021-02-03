b2b935f99afcc5e255e90bf5de869316fbeecfbb
eligolf <59540119+eligolf@users.noreply.github.com>
2021-02-03 06:32:16 +0100

    Update pve_compressed_just_ai.py

--- pve_compressed_just_ai.py
+++ pve_compressed_just_ai.py
@@ -1,14 +1,11 @@
 def e(b,t):return any([b[i]==b[i+d]==b[i+2*d]==t for i,d in((0,1),(3,1),(6,1),(0,3),(1,3),(2,3),(0,4),(2,2))])
 def n(b,t,d=8):
-    q=0
-    if e(b,t):q=(0,1)
-    if e(b,-t):q=(0,-1)
-    if all(b):q=(0,0)
-    if q:return q
+    if e(b,t):return 0,1
+    if e(b,-t):return 0,-1
+    if all(b):return 0,0
     x=-2
     for m in range(9):
         if b[m]==0:
             b[m]=t;s,b[m]=-n(b,-t,d-1)[1],0
             if s>x:x,y=s,m
     return y,x
-
