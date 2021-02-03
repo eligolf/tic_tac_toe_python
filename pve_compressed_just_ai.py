dfb15e07984f7fdd33ef25e57e293ae2329d807c
eligolf <59540119+eligolf@users.noreply.github.com>
2021-02-03 06:21:25 +0100

    Add files via upload

---
+++ pve_compressed_just_ai.py
@@ -0,0 +1,12 @@
+def e(b,t):return any([b[i]==b[i+d]==b[i+2*d]==t for i,d in((0,1),(3,1),(6,1),(0,3),(1,3),(2,3),(0,4),(2,2))])
+def n(b,d=8,t):
+    if e(b,t):return 0,1
+    if e(b,-t):return 0,-1
+    if all(b):return 0,0
+    x=-2
+    for m in range(9):
+        if b[m]==0:
+            b[m]=t;s,b[m]=-n(b,d-1,-t)[1],0
+            if s>x:x,y=s,m
+    return y,x
+
