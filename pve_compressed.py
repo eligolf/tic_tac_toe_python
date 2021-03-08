64c35ea22092236a6e936eff91bbaed9f1c962cd
eligolf <59540119+eligolf@users.noreply.github.com>
2021-03-08 08:50:21 +0100

    Update pve_compressed.py

--- pve_compressed.py
+++ pve_compressed.py
@@ -1,12 +1,12 @@
 def e(b,t):return any([b[i]==b[i+d]==b[i+2*d]==t for i,d in((0,1),(3,1),(6,1),(0,3),(1,3),(2,3),(0,4),(2,2))])
-def n(b,d,t):
+def n(b,t):
     if e(b,t):return 0,1
     if e(b,-t):return 0,-1
     if all(b):return 0,0
     x=-2
     for m in range(9):
         if b[m]==0:
-            b[m]=t;s,b[m]=-n(b,d-1,-t)[1],0
+            b[m]=t;s,b[m]=-n(b,-t)[1],0
             if s>x:x,y=s,m
     return y,x
 b,w=[0]*9,1
@@ -18,4 +18,4 @@ while 1:
         if u.isdigit():
             u=int(u)-1
             if u<9and not b[u]:b[u],w=-1,-1
-    else:m,b[m],w=n(b,8,1)[0],1,1
+    else:m,b[m],w=n(b,1)[0],1,1
